acfa7be400cdbedc1b505f85c6442071928ac69d
Edh41 <eddiehaggerty41@gmail.com>
2025-03-07 10:26:07 -0700

    Adding new effects

--- Assets/Scripts/ItemEffect/BigRick.cs
+++ Assets/Scripts/ItemEffect/BigRick.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using CreatorKitCode;
+using Unity.VisualScripting;
+using UnityEditor;
+using System.Drawing;
+using CreatorKitCodeInternal;
 
 public class BigRick : UsableItem.UsageEffect
 {
     public override bool Use(CharacterData user)
     {
-        return false;
+        return true;
     }
 }
